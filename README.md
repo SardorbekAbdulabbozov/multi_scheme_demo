c8d4c9c5077c90c5d572dadd35a8cd3db1c246d2
Sardorbek Abdulabbozov <sabdulabbosov@gmail.com>
2025-12-20 15:44:22 +0500

    initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+This app demonstrates implementing multi-scheme/flavor setup to develop white-labelled apps with
+single codebase
---
+++ android/app/src/main/kotlin/com/abdulabbozov/demos/multi_scheme_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.abdulabbozov.demos.multi_scheme_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
